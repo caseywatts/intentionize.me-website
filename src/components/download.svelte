e7f851279c031fbbc514145e246d5d5fdadea5b6
Casey Watts <casey.s.watts@gmail.com>
2022-07-01 21:10:15 -0400

    refactor into components

---
+++ src/components/download.svelte
@@ -0,0 +1,7 @@
+<div class="bg-violet-300 border-2 border-violet-600 md:absolute mt-6 bottom-0 w-full">
+  <div class="p-8 pt-6 space-y-2">
+    <div><a class="bg-violet-500 text-violet-100 p-3 text-lg rounded" href="https://github.com/caseywatts/intentions/releases/latest/download/intentions-mac.zip">download intentions for mac</a></div>
+    <div class="pt-2 underline text-blue-800"><a href="https://github.com/caseywatts/intentions/">free and open source</a></div>
+    <div class="underline text-blue-800">made by <a href="https://twitter.com/heycaseywattsup">Casey Watts!</a></div>
+  </div>
+</div>
---
+++ src/components/two-steps.svelte
@@ -0,0 +1,29 @@
+<script>
+  import { base } from '$app/paths';
+</script>
+
+<div class="md:flex justify-items-center justify-around items-stretch p-2 space-y-6">
+  <div class="bg-violet-300 m-3 rounded-lg border-2 border-violet-600 shadow-lg shadow-violet-900">
+    <div class="bg-violet-500 text-violet-100 p-4 text-xl">one</div>
+    <div class="p-4">
+      <div class="p-3 text-xl">
+        <div>
+          press
+          <pre class="inline bg-violet-200 p-2">control</pre>
+          +
+          <pre class="inline bg-violet-200 p-2">~</pre>
+        </div>
+        <div class="p-2">then type your intention</div>
+      </div>
+      <img class="inline-block w-96" src="{base}/intentions window.png" />
+    </div>
+  </div>
+    
+  <div class="bg-violet-300 m-3 rounded-lg border-2 border-violet-600 shadow-lg shadow-violet-900">
+    <div class="bg-violet-500 text-violet-100 p-4 text-xl">two</div>
+    <div class=" p-4">
+      <div class="p-3 text-xl">Now your menubar will remind you 🎉</div>
+      <img class="inline-block w-96" src="{base}/intentions menubar.png" />
+    </div>
+  </div>
+</div>
