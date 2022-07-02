26f90dc469192bd6fd560a95c1e1acbf7fa86b9a
Casey Watts <casey.s.watts@gmail.com>
2022-07-01 21:58:51 -0400

    fade into background the less important elements

--- src/components/menu-bar.svelte
+++ src/components/menu-bar.svelte
@@ -3,18 +3,18 @@
   export let myIntention = "do the dishes";
 </script>
 
-<div class="px-4 py-2 flex justify-between bg-violet-800 text-white fill-white stroke-white">
+<div class="px-4 py-2 flex justify-between bg-violet-800 text-violet-400 stroke-violet-400">
   <div class="flex">
-    <span class="px-3">Finder</span>
-    <span class="px-3">File</span>
-    <span class="px-3">Edit</span>
-    <span class="px-3">View</span>
-    <span class="px-3">Go</span>
-    <span class="px-3">Window</span>
+    <span class="px-3 font-semibold text-violet-50">Intentions in your Menubar</span>
+    <span class="px-3 text-violet-400">File</span>
+    <span class="px-3 text-violet-400">Edit</span>
+    <span class="px-3 text-violet-400">View</span>
+    <span class="px-3 text-violet-400">Go</span>
+    <span class="px-3 text-violet-400">Window</span>
   </div>
   <div class="flex">
-    <span class=""><Basicon iconName="volume-up"/></span>
-    <span class="pl-2 pr-3">{myIntention}</span>
+    <span class="stroke-violet-50"><Basicon iconName="volume-up"/></span>
+    <span class="pl-2 pr-3 text-violet-50">{myIntention}</span>
     <span class="px-3"><Basicon iconName="desktop-computer-mac"/></span>
     <span class="px-3"><Basicon iconName="moon-night"/></span>
     <span class="px-3"><Basicon iconName="volume-up"/></span>
